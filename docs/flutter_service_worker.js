fe3a4ec0cc41897ae13483b7055bdef439962aed
Andrésenou <andresenou03@gmail.com>
2025-01-25 10:26:59 +0100

    Rebuild and deploy new content to docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "851756f01b8480caa01276f9dae2cb4a
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "9f623d983a5bea6248199d6d885e6ec1",
+"flutter_bootstrap.js": "0f8c58c3503a7508afd442b464a2c4cc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
